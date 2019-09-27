48fd13ed403860f4e9695caecf3b2e72741aecf1
myalisa <myalisamiro@gmail.com>
2019-09-27 11:09:27 -0500

    added_gem

--- app/controllers/api/food_controller.rb
+++ app/controllers/api/food_controller.rb
@@ -1,6 +1,6 @@
 class Api::FoodController < ApplicationController
   def index
-    response = HTTP.get("https://www.food2fork.com/api/search?key=f7cad8508326076cad8c94a1c645c981&q=#{params[:search]}")
+    response = HTTP.get("https://www.food2fork.com/api/search?key=f7cad8508326076cad8c94a1c645c981&q=#{params[:search]}&apiKey=#{ENV['API_KEY']}")
     @food = response.parse['food']
     render json: @food
   end
