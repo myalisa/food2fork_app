5f798be98b7c5fbf2fb208dfeaedb61061a92c51
myalisa <myalisamiro@gmail.com>
2019-09-27 11:02:31 -0500

    Food2Fork_app

---
+++ app/controllers/api/food_controller.rb
@@ -0,0 +1,7 @@
+class Api::FoodController < ApplicationController
+  def index
+    response = HTTP.get("https://www.food2fork.com/api/search?key=f7cad8508326076cad8c94a1c645c981&q=#{params[:search]}")
+    @food = response.parse['food']
+    render json: @food
+  end
+end
